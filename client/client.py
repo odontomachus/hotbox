004482e6e95c0e8bb994f95d6afd20d4e0b08f9d
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2015-03-21 22:42:03 -0400

    Fixed byte readings for signed bytes.

--- client/client.py
+++ client/client.py
@@ -1,24 +1,28 @@
+from collections import defaultdict
+import struct
+
 import serial
+from serial import SerialException
 
 PORT="/dev/ttyUSB0"
 
 s = serial.Serial(PORT, 9600, timeout=0.5)
 
-def listen():
-        data = []
-        s.write(b"s")
-        while True:
-            try:
-                message = s.read(30)
-                row = {}
-                parse_message(message, row)
-                data.append(row)
-                if row:
-                    print(row)
-            except:
-                pass
+keys = ['Time left', 'T1', 'T2', 'Off goal', 'Temp diff', 'Part', 'State', 'Cycle']
 
-test = "MSG:5:T\x55\x33C\x08\xa1P\x0fY\x08S\x20:EOM"
+def listen():
+    print("\t".join(keys))
+    while True:
+        try:
+            message = s.read(30)
+            row = defaultdict(str)
+            parse_message(message, row)
+            if row:
+                    print("\t".join(map(lambda key: str(row[key]), keys)))
+        except SerialException as e:
+            pass
+        except KeyboardInterrupt:
+            break
 
 def parse_message(message, data):
     while len(message) > 4 and message[0:4] != b"MSG:":
@@ -46,10 +50,10 @@ def parse_message(message, data):
                 data['State'] = "On" if message[1] != '\x00' else "Off"
                 message = message[2:]
             elif message[0] == b'G'[0]:
-                data['Off goal'] = message[1]
+                data['Off goal'] = struct.unpack('b', message[1:2])[0]
                 message = message[2:]
             elif message[0] == b'D'[0]:
-                data['Temp diff'] = message[1]
+                data['Temp diff'] = struct.unpack('b', message[1:2])[0]
                 message = message[2:]
             elif message == b":EOM":
                 return True
