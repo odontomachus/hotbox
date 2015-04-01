00a9a57b6f6ad80c54967481909bafb79738a45d
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2015-04-01 12:29:12 -0400

    interface shell.

--- client/gui/__init__.py
+++ client/gui/__init__.py
@@ -13,18 +13,50 @@ class Logger(tk.Frame):
         pass
 
 class Config(tk.Frame):
-    def __init__(self, controller, *args, **kwargs):
+    def __init__(self, done, controller, *args, **kwargs):
+        """ Configuration panel and actions.
+
+        @param done: callback to run when finished configurationt task.
+        @param controller: serial client to send configuration to.
+        """
+
+        self.done = done
         self.controller = controller
         super().__init__(*args, **kwargs)
-        self.port='COM4'
-        # @TODO port
-        # output file
-        # temp 30 - 145
-        # time 0 - 3600*9
-        self.save = tk.Button(self, text="Save", command = self.configure)
+
+        self.v_port = tk.StringVar()
+        self.v_temp = tk.IntVar()
+        self.v_time = tk.IntVar()
+
+        self.port = 'COM4'
+        self.time = 6*3600
+        self.temp = 53
+        self.v_port.set(self.port)
+        self.v_time.set(self.time)
+        self.v_temp.set(self.temp)
+
+        tk.Label(self, text="Serial Port").grid(column=0, row=0)
+        tk.Entry(self, exportselection=0, textvariable=self.v_port).grid(column=1, row=0)
+
+        tk.Label(self, text="Heating Temperature").grid(column=0, row=1)
+        tk.Entry(self, exportselection=0, textvariable=self.v_temp).grid(column=1, row=1)
+
+        tk.Label(self, text="Heating Time").grid(column=0, row=2)
+        tk.Entry(self, exportselection=0, textvariable=self.v_time).grid(column=1, row=2)
+
+        tk.Button(self, text="Cancel", command = self.done).grid(column=0,row=3)
+        tk.Button(self, text="Save", command = self.configure).grid(column=1,row=3)
 
     def configure(self):
-        controller.configure(self.time, self. temp)
+        self.port = self.v_port.get()
+        self.time = self.v_time.get()
+        self.temp = self.v_temp.get()
+        controller.configure(self.time, self.temp)
+        self.done()
+
+    def update_config(self, config):
+        self.time = config.time
+        self.temp = config.temp
 
 class App:
     def __init__(self):
@@ -34,15 +66,22 @@ class App:
         root.wm_geometry('800x600')
         self.root = root
 
+        self.menu = tk.Menu(root)
+        self.file_menu = tk.Menu(self.menu)
+        self.menu.add_cascade(label='File', menu=self.file_menu)
+        self.file_menu.add_command(label='Configure', command=self.configure)
+        root['menu'] = self.menu
+
         self.logger = Logger()
         self.display = Display(self.logger)
 
         # setup client
         self.controller = Client()
+        self.config = Config(self.main, self.controller)
+
         self.controller.register(('status',), self.state_change)
         self.controller.register(('run_status',), self.display.run_message)
-
-        self.config = Config(self.controller)
+        self.controller.register(('config',), self.config.update_config)
 
         # connect client
         self.controller.connect(self.config.port)
@@ -57,10 +96,13 @@ class App:
 
     def configure(self):
         """ Show configuration panel """
-        pass
+        self.controls.grid_forget()
+        self.display.grid_forget()
+        self.config.grid()
 
-    def run(self):
-        """ Show run info panel """
-        self.status_frame = sf = tk.Frame(self.root)
+    def main(self):
+        self.config.grid_forget()
+        self.controls.grid()
+        self.display.grid()
 
         
